cfecb5648d692f664b7192390688dd01b34e52db
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2023-11-22 00:48:48 +0300

    Fix

--- app/routes/accounting.js
+++ app/routes/accounting.js
@@ -3,24 +3,26 @@ import { Database } from 'common/databases/PostgreSQL/PostgreSQL.js';
 
 async function register(app, options)
 {
-    app.get("/accounting", { config: { access: "ceo" } }, async (req, res) =>
+    app.get("/accounting", { config: { access: [ "ceo", "curator" ] } }, async (req, res) =>
     {
         const DAY_TO_MS = 24 * 60 * 60 * 1000;
-        const data = await Database.execute(`SELECT * FROM accounting INNER JOIN streamers ON streamer_id = id ORDER BY id ASC, day ASC`);
+        const data = await Database.execute(`SELECT * FROM accounting RIGHT JOIN streamers ON streamer_id = id ORDER BY id ASC, day ASC`);
 
         const streamers = [ ];
-        const minDay = Math.min(...data.map(e => e.day));
-        const maxDay = Math.max(...data.map(e => e.day));
-        const total_days = (maxDay - minDay) / DAY_TO_MS + 1;
+        const maxDay = new Date();
+        const minDay = Math.min(...data.map(e => e.day).filter(e => e));
+        const total_days = Math.floor((maxDay - minDay) / DAY_TO_MS) + 1;
         const days = new Array(total_days).fill(null).map((_, day) => new Date(minDay + day * DAY_TO_MS));
         for (let i = 0; i < data.length; )
         {
             function getData(day)
             {
-                if (data[i]?.day.getTime() == day.getTime()) return data[i++];
+                if (data[i]?.day?.getTime() == day.getTime()) return data[i++];
                 return { day, time: new Interval(0), diamonds: 0 };
             }
-            streamers.push(Object.assign(days.map(getData), data[i - 1]));
+            const first_index = i;
+            streamers.push(Object.assign(days.map(getData), data[first_index]));
+            if (first_index == i) i++;
         }
         return res.render("general/layout.ejs", { template: "accounting", days, streamers });
     });
@@ -43,7 +45,7 @@ async function register(app, options)
             }
         }
     };
-    app.post("/accounting", { schema: POST_SCHEMA, config: { authentication: true, access: "admin" } }, async (req, res) =>
+    app.post("/accounting", { schema: POST_SCHEMA, config: { authentication: true, access: [ "admin", "curator" ] } }, async (req, res) =>
     {
         const batch = new Database.AnonymousBatch();
         batch.execute("TRUNCATE TABLE accounting");
