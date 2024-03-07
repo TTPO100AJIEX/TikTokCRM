fe50448fde997f97180e369e26cc613e5534f3fd
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2024-03-07 23:40:40 +0300

    Add employees.streams[] to db

--- database/scripts/fill/employees.sql
+++ database/scripts/fill/employees.sql
@@ -1,3 +1,3 @@
-INSERT INTO employees (login, password, access, responsibility) VALUES
-    ('TTPO100AJIEX', '$2b$11$fwVt/mpqY.zStXj61IQv6OOb21VFzY3942gZ2QLPAby5d.cdwgn/e', 'ADMIN', 'GROUP_1'),
-    ('Tester!', '$2b$11$fwVt/mpqY.zStXj61IQv6OOb21VFzY3942gZ2QLPAby5d.cdwgn/e', 'CURATOR', 'GROUP_1')
+INSERT INTO employees (login, password, access, streams, responsibility) VALUES
+    ('TTPO100AJIEX', '$2b$11$fwVt/mpqY.zStXj61IQv6OOb21VFzY3942gZ2QLPAby5d.cdwgn/e', 'ADMIN', '{ STREAM_1, STREAM_2 }', 'GROUP_1'),
+    ('Tester!', '$2b$11$fwVt/mpqY.zStXj61IQv6OOb21VFzY3942gZ2QLPAby5d.cdwgn/e', 'CURATOR', '{ STREAM_3 }', 'GROUP_1')
