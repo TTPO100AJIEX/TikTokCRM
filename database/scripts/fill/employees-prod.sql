33c483c6df9eb89bbdf06abe08f85002be8c53f5
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2024-03-27 23:34:26 +0300

    Dump

---
+++ database/scripts/fill/employees-prod.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (login, password, access, streams, responsibilities) VALUES
+    ('MainAdmin', '$2b$11$u8yfwSL.TdeqyYQyHbayzumJ9gxZd9rDM.ZKhxyDDPJ508/MU8Idq', 'ADMIN', '{}', '{}');
